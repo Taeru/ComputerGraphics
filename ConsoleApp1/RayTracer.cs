46fa513045ea4d8d023c9c03e6982d8ad35e8bee
suwun <suwun@DESKTOP-TJ5UIOA>
2024-04-03 14:59:01 +0900

    antialiasing

--- ConsoleApp1/RayTracer.cs
+++ ConsoleApp1/RayTracer.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-using System.Drawing;
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+
+
 
 namespace RayTracer
 {
     class Program
     {
+        static GameWindow window;
+        static Random rand = new Random();
         static void Main(string[] args)
         {
             // 창 생성 및 OpenGL 초기화
-            GameWindow window = new GameWindow(512, 512, GraphicsMode.Default, "Ray Tracer");
+            window = new GameWindow(512, 512, GraphicsMode.Default, "Ray Tracer");
             window.Load += Window_Load;
             window.RenderFrame += Window_RenderFrame;
             window.Run();
@@ -18,108 +22,131 @@ namespace RayTracer
 
         private static void Window_Load(object sender, EventArgs e)
         {
-            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            GL.ClearColor(0f, 0f, 0f, 0f);
+
         }
 
         private static void Window_RenderFrame(object sender, FrameEventArgs e)
         {
-            GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            // 카메라 설정
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            //int N = 64;
+            int N = 1;
             Vector3 eyePoint = new Vector3(0, 0, 0);
             Vector3 u = new Vector3(1, 0, 0);
             Vector3 v = new Vector3(0, 1, 0);
             Vector3 w = new Vector3(0, 0, 1);
-            float l = -0.1f, r = 0.1f, b = -0.1f, t = 0.1f, d = 0.1f;
+            float l = -0.1f;
+            float r = 0.1f;
+            float b = -0.1f;
+            float t = 0.1f;
+            float d = 0.1f;
 
-            // 장면 객체 정의
+
             Plane plane = new Plane(new Vector3(0, -2, 0), new Vector3(0, 1, 0));
             Sphere sphere1 = new Sphere(new Vector3(-4, 0, -7), 1);
             Sphere sphere2 = new Sphere(new Vector3(0, 0, -7), 2);
             Sphere sphere3 = new Sphere(new Vector3(4, 0, -7), 1);
 
-            // 광원 설정
             Vector3 lightPos = new Vector3(-4, 4, -3);
-
-            // 각 픽셀에 대한 레이 트레이싱 수행
+            GL.Begin(PrimitiveType.Points);
             for (int y = 0; y < 512; y++)
             {
                 for (int x = 0; x < 512; x++)
                 {
-                    // 픽셀 중심을 통과하는 광선 계산
-                    Vector3 rayDir = CalculateRayDirection(x, y, 512, 512, l, r, b, t, d, u, v, w);
 
-                    // 광선과 객체의 교점 계산
-                    float minDist = float.MaxValue;
-                    Object closestObj = null;
-                    if (plane.Intersect(eyePoint, rayDir, out float t))
+                    
+                    Vector3 color = Vector3.Zero;
+                    for (int s= 0; s<N; s++)
                     {
-                        if (t < minDist)
+                        float rx = (float)rand.NextDouble();
+                        float ry = (float)rand.NextDouble();
+
+                        Vector3 rayDir = CalculateRayDirection(x, y, rx, ry, 512, 512, l, r, b, t, d, u, v, w);
+
+                        float minDist = float.MaxValue;
+                        Object closestObj = null;
+                        if (plane.Intersect(eyePoint, rayDir, out t))
                         {
-                            minDist = t;
-                            closestObj = plane;
+                            if (t < minDist)
+                            {
+
+                                minDist = t;
+                                closestObj = plane;
+                            }
+                        }
+                        if (sphere1.Intersect(eyePoint, rayDir, out t))
+                        {
+                            if (t < minDist)
+                            {
+                                minDist = t;
+                                closestObj = sphere1;
+                            }
+                        }
+                        if (sphere2.Intersect(eyePoint, rayDir, out t))
+                        {
+                            if (t < minDist)
+                            {
+                                minDist = t;
+                                closestObj = sphere2;
+
+                            }
+                        }
+
+                        if (sphere3.Intersect(eyePoint, rayDir, out t))
+                        {
+                            if (t < minDist)
+                            {
+                                minDist = t;
+                                closestObj = sphere3;
+                            }
+                        }
+
+                        // 교점에 따른 픽셀 색상 설정
+                        if (closestObj != null)
+                        {
+                            Vector3 hitPoint = eyePoint + rayDir * minDist;
+                            Vector3 normal = closestObj.GetNormal(hitPoint);
+                            color = PhongShading(closestObj, hitPoint, normal, lightPos);
+
+                            //Vector3 color = new Vector3(0.0f, 0.0f, 0.0f);
+                            GL.Color3(color);
+
+                        }
+                        else
+                        {
+
+                            GL.Color3(1.0f, 1.0f, 1.0f);
                         }
                     }
-                    if (sphere1.Intersect(eyePoint, rayDir, out t))
-                    {
-                        if (t < minDist)
-                        {
-                            minDist = t;
-                            closestObj = sphere1;
-                        }
-                    }
-                    if (sphere2.Intersect(eyePoint, rayDir, out t))
-                    {
-                        if (t < minDist)
-                        {
-                            minDist = t;
-                            closestObj = sphere2;
-                        }
-                    }
+                    
 
-                    if (sphere3.Intersect(eyePoint, rayDir, out t))
-                    {
-                        if (t < minDist)
-                        {
-                            minDist = t;
-                            closestObj = sphere3;
-                        }
-                    }
-
-                    // 교점에 따른 픽셀 색상 설정
-                    if (closestObj != null)
-                    {
-                        Vector3 hitPoint = eyePoint + rayDir * minDist;
-                        Vector3 normal = closestObj.Normal(hitPoint);
-                        Vector3 color = PhongShading(closestObj, hitPoint, normal, lightPos);
-                        GL.Color3(color);
-                    }
-                    else
-                    {
-                        GL.Color3(0.0f, 0.0f, 0.0f); // 배경색
-                    }
-
-                    GL.Vertex2(x / 256.0f - 1.0f, 1.0f - y / 256.0f);
+                    GL.Vertex2(x / 256.0f - 1.0f, y / 256.0f- 1.0f);
                 }
             }
-
+            GL.End();
+           
             window.SwapBuffers();
         }
 
-        private static Vector3 CalculateRayDirection(int x, int y, int width, int height, float l, float r, float b, float t, float d, Vector3 u, Vector3 v, Vector3 w)
+        private static Vector3 CalculateRayDirection(int x, int y, float rx, float ry, int width, int height, float l, float r, float b, float t, float d, Vector3 u, Vector3 v, Vector3 w)
         {
-            float px = (x + 0.5f) / width;
-            float py = (y + 0.5f) / height;
-            Vector3 rayDir = u * (l + (r - l) * px) + v * (b + (t - b) * py) - w * d;
+            float px = l + (r - l) * (x + rx) / width;
+            float py = b + (t - b) * (y + ry) / height;
+            
+            Vector3 rayDir = -w * d + u * px + v * py;
+
             return rayDir.Normalized();
         }
 
         private static Vector3 PhongShading(Object obj, Vector3 hitPoint, Vector3 normal, Vector3 lightPos)
         {
-            Vector3 ka, kd, ks;
-            float specPower;
+            Vector3 ka = Vector3.Zero;
+            Vector3 kd = Vector3.Zero;
+            Vector3 ks = Vector3.Zero;
+            float specPower = 0;
 
-            // 물체별 재질 파라미터 설정
             if (obj is Plane)
             {
                 ka = new Vector3(0.2f, 0.2f, 0.2f);
@@ -156,23 +183,20 @@ namespace RayTracer
                 throw new Exception("Invalid object type");
             }
 
-            // 광원 벡터 계산
             Vector3 lightDir = (lightPos - hitPoint).Normalized();
 
-            // Diffuse 성분 계산
             float diffuseTerm = Math.Max(Vector3.Dot(lightDir, normal), 0);
             Vector3 diffuse = kd * diffuseTerm;
 
-            // Specular 성분 계산
             Vector3 viewDir = (-hitPoint).Normalized();
             Vector3 reflectDir = 2 * Vector3.Dot(normal, lightDir) * normal - lightDir;
             float specularTerm = (float)Math.Pow(Math.Max(Vector3.Dot(viewDir, reflectDir), 0), specPower);
             Vector3 specular = ks * specularTerm;
 
-            // 최종 색상 계산
+
             Vector3 color = ka + diffuse + specular;
 
-            // Gamma 보정
+
             color.X = (float)Math.Pow(color.X, 1.0 / 2.2);
             color.Y = (float)Math.Pow(color.Y, 1.0 / 2.2);
             color.Z = (float)Math.Pow(color.Z, 1.0 / 2.2);
@@ -184,7 +208,7 @@ namespace RayTracer
     abstract class Object
     {
         public abstract bool Intersect(Vector3 eyePoint, Vector3 rayDir, out float t);
-        public abstract Vector3 Normal(Vector3 point);
+        public abstract Vector3 GetNormal(Vector3 point);
     }
 
     class Plane : Object
@@ -212,7 +236,7 @@ namespace RayTracer
             return false;
         }
 
-        public override Vector3 Normal(Vector3 point)
+        public override Vector3 GetNormal(Vector3 point)
         {
             return Normal;
         }
@@ -231,12 +255,16 @@ namespace RayTracer
 
         public override bool Intersect(Vector3 eyePoint, Vector3 rayDir, out float t)
         {
-            Vector3 ec = eyePoint - Center;
-            float a = Vector3.Dot(rayDir, rayDir);
-            float b = 2 * Vector3.Dot(ec, rayDir);
-            float c = Vector3.Dot(ec, ec) - Radius * Radius;
+            Vector3 p = eyePoint;
+            Vector3 d = rayDir;
+            Vector3 m = p - Center;
+            
+            float p_d = Vector3.Dot(m, d);
+            float p_p = Vector3.Dot(m, m);
 
-            float discriminant = b * b - 4 * a * c;
+
+            float discriminant = p_d * p_d - (p_p - Radius * Radius);
+
             if (discriminant < 0)
             {
                 t = 0;
@@ -244,22 +272,37 @@ namespace RayTracer
             }
             else
             {
-                float e = (float)Math.Sqrt(discriminant);
-                float denom = 2 * a;
-                t = (-b - e) / denom;
-                if (t > 0)
+                float sqrt_discriminant = (float)Math.Sqrt(discriminant);
+                float t1 = (-p_d - sqrt_discriminant);
+                float t2 = (-p_d + sqrt_discriminant);
+
+                if (t1 > t2)
                 {
-                    return true;
+                    float temp = t1;
+                    t1 = t2;
+                    t2 = temp;
                 }
 
-                t = (-b + e) / denom;
-                return (t > 0);
+                if (t1 < 0)
+                {
+                    t1 = t2;
+                    if (t1 < 0)
+                    {
+                        t = 0;
+                        return false;
+                    }
+                }
+               
+                t = t1;
+                return true;
             }
         }
 
-        public override Vector3 Normal(Vector3 point)
+
+        public override Vector3 GetNormal(Vector3 point)
         {
             return (point - Center).Normalized();
         }
     }
 }
+
